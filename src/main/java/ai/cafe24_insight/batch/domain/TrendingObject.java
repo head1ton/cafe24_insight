53f78d2003e747a23ace553744ecafa706b50ef5
Hwan / CTO <head1ton@gmail.com>
2023-09-19 19:40:16 +0900

    Story/001 (#1)
    * base settings
    
    * test

---
+++ src/main/java/ai/cafe24_insight/batch/domain/TrendingObject.java
@@ -0,0 +1,20 @@
+package ai.cafe24_insight.batch.domain;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Getter
+@ToString
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class TrendingObject {
+
+    private TrendingMovie[] results;
+
+    @Builder
+    public TrendingObject(TrendingMovie[] results) {
+        this.results = results;
+    }
+}
---
+++ src/main/java/ai/cafe24_insight/controller/WebServerController.java
@@ -0,0 +1,24 @@
+package ai.cafe24_insight.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+
+@Slf4j
+@RestController
+public class WebServerController {
+
+    @GetMapping("/test")
+    public Mono<String> doTest() {
+        WebClient webClient = WebClient.builder().build();
+        return webClient.get()
+                        .uri("https://shoppingtip.cafe24api.com/api/v2/admin/store?shop_no=1")
+                        .accept(MediaType.APPLICATION_JSON)
+                        .header("Authorization", "Bearer s1yhtGChBRTrH2aIEGNNjB")
+                        .retrieve()
+                        .bodyToMono(String.class);
+    }
+}
