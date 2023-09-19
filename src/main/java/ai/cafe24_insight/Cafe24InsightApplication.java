85ecf661db04d62f13a7e069ca7e2b27fba3a4c8
head1ton <head1ton@gmail.com>
2023-09-19 11:05:07 +0900

    Initial commit

---
+++ src/main/java/ai/cafe24_insight/Cafe24InsightApplication.java
@@ -0,0 +1,13 @@
+package ai.cafe24_insight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cafe24InsightApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Cafe24InsightApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ai/cafe24_insight/Cafe24InsightApplicationTests.java
@@ -0,0 +1,13 @@
+package ai.cafe24_insight;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cafe24InsightApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
